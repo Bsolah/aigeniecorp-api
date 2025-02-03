98c582584d0b60c09c86ef1820a77b07289f062d
Bsolah <bisola@niyis.co.uk>
2025-02-03 09:12:26 +0000

    git add .

--- src/public/assets/LandingPages-C4oo3AJn.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-BWq_bGw4.js";const e=()=>n.jsx(n.Fragment,{children:"Langing Page in progress"});export{e as default};
---
+++ src/public/assets/LandingPages-CKkN_qif.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-g26pDq1l.js";const e=()=>n.jsx(n.Fragment,{children:"Langing Page in progress"});export{e as default};
