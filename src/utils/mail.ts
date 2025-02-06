df53063cd670e9f29feee9fe18185f5a36f4cf36
Popoola_Sinaayo <olusegunpopoola4real@gmail.com>
2025-02-06 21:19:14 +0100

    update on code

--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -1,23 +1,23 @@
-import sgMail from '@sendgrid/mail';
+// import sgMail from '@sendgrid/mail';
 import dotenv from 'dotenv';
 // import { BadRequest } from '../errors';
 dotenv.config(); // Load .env file with SENDGRID_API_KEY
 
-const sendMail = async (to: string, subject: string, html: string) => {
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
-  const msg = {
-    to, // Change to your recipient
-    from: 'bisola@niyis.co.uk', // Change to your verified sender
-    subject,
-    html,
-  };
+// const sendMail = async (to: string, subject: string, html: string) => {
+//   sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
+//   const msg = {
+//     to, // Change to your recipient
+//     from: 'bisola@niyis.co.uk', // Change to your verified sender
+//     subject,
+//     html,
+//   };
 
-  try {
-    await sgMail.send(msg);
-  } catch (error) {
-    console.log(error);
-    // throw new BadRequest('Error sending email');
-  }
-};
+//   try {
+//     await sgMail.send(msg);
+//   } catch (error) {
+//     console.log(error);
+//     // throw new BadRequest('Error sending email');
+//   }
+// };
 
-export default sendMail;
+// export default sendMail;
