799a9bb969f7cbdbdf159c8818e3de34e069bb23
Popoola_Sinaayo <olusegunpopoola4real@gmail.com>
2025-02-07 12:33:12 +0100

    update on send mail

--- src/controllers/leadController.ts
+++ src/controllers/leadController.ts
@@ -1,23 +1,23 @@
 import { Request, Response, NextFunction } from "express";
 import Lead from "../models/Lead";
-// import sendMail from "../utils/mail";
-// import welcomeMail from "../views/welcome";
+import sendMail from "../utils/mail";
+import welcomeMail from "../views/welcome";
 
 export const createLead = async (req: Request, res: Response) => {
   try {
     const leadBody = req.body;
     const lead = new Lead(leadBody);
     await lead.save();
-    // await sendMail(
-    //   lead.email,
-    //   "Welcome to our platform",
-    //   welcomeMail(lead.firstName),
-    // );
-    // await sendMail(
-    //   "bisola@niyis.co.uk",
-    //   "New lead",
-    //   welcomeMail(lead.firstName),
-    // );
+    await sendMail(
+      lead.email,
+      "Welcome to our platform",
+      welcomeMail(lead.firstName),
+    );
+    await sendMail(
+      "bisola@niyis.co.uk",
+      "New lead",
+      welcomeMail(lead.firstName),
+    );
     res.status(201).send({ message: "Lead uploaded successfully", data: null });
   } catch (error: any) {
     res.status(500).send(error.message);
