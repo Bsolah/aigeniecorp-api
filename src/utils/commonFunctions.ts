6788b0bfc4a45a6c2686e361ae624aafe4be99f7
Bsolah <bisola@niyis.co.uk>
2025-02-11 18:14:57 +0000

    add use case for financial compliance

--- src/utils/commonFunctions.ts
+++ src/utils/commonFunctions.ts
@@ -99,11 +99,19 @@ export const convertToStructuredObject = (inputString: string, internalAI: strin
 
 
   // SENTITIVE HARD CODING 1
-  if ((internalAI == "knb") && (externalAI == "dai") && content.toLocaleLowerCase().includes("capital of us")) {
-    response = "There appear to be a discrepancy between your internal and external and suggests to correct the information",
+  if ((internalAI == "knb") && (externalAI == "dai") && content.toLocaleLowerCase().includes("cash")) {
+    response = `:warning: There appears to be a discrepancy in your knowledge base. In the United States, the federal reporting threshold for cash transactions is $10,000, as mandated by the Bank Secrecy Act (BSA). This applies to two primary scenarios:
+      Financial Institutions under which your "Test Company" falls: Banks and other financial entities must file a Currency Transaction Report (CTR) with the Financial Crimes Enforcement Network (FinCEN) for cash transactions exceeding $10,000 in a single business day.
+      Source: FinCEN CTR Requirements.
+      Businesses (Non-Financial Sectors): Businesses (e.g., retailers, car dealers) receiving cash payments over $10,000 must file IRS Form 8300.
+      Source: IRS Form 8300 Guidance.
+      
+      Please refer to the audit trail below that you can extract:
+      V1. Created by "John Dow" 17.02.2020 and approved by "Alicia Johnson"  01.02.2020
+      V2. Updated by "Alex Thompson "20.12.2024" and approved by "Chloe Warren" 20.12.2024`,
       console.log("both LLM")
-  } else if ((internalAI === "knb") && content.toLocaleLowerCase().includes("capital of us")) {
-    response = "The capital of USA is Califonia",
+  } else if ((internalAI === "knb") && content.toLocaleLowerCase().includes("cash")) {
+    response = "Based on your company knowledge base, in the article 'Company Compliance' the reporting threshold for cash transaction in the US is 50000 USD.",
       console.log("internal LLM")
   } else {
     console.log("circle back")
@@ -120,14 +128,16 @@ export const convertToStructuredObject = (inputString: string, internalAI: strin
         Please submit this request through our internal AI model, in line with our security policy.`;
     console.log("one external LLM")
   } else if ((internalAI == "knb") && content.toLocaleLowerCase().includes("follow-up email")) {
-    response = `  Subject: Follow-Up & Exclusive Offer for Acme Corp 
-                  Hi Acme Corp,
-                  I hope you’re well. I wanted to follow up on our recent discussion and congratulate your team on achieving an impressive Q3 revenue of $2.5M. Please extend my congratulations to Jane Doe and the entire Acme Corp team for this milestone.
+    response = ` Subject: Follow-Up & Exclusive Offer for Acme Corp 
+    Hi Acme Corp,
+    I hope you’re well. I wanted to follow up on our recent discussion and congratulate your team on achieving an impressive Q3 revenue of $2.5M. Please extend my congratulations to Jane Doe and the entire Acme Corp team for this milestone.
                   To support your continued success, we’re excited to offer you an exclusive 10% discount under our YC2025 promotion. Let’s connect soon to discuss how we can best serve your upcoming needs.
                   Looking forward to your thoughts.
                   Best regards,
                   Kawtar Lahlou
-                  Company Test, CEO & Co-founder`
+                  Company Test, CEO & Co-founder`;
+    console.log("one internal LLM")
+  } else {
     console.log("circle back")
   }
 
